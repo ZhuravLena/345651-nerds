acac4dcac5809a7f227ab3b12fd043d90a9abbfa
ZhuravLena <ezh27@yandex.ru>
2017-02-17 19:25:48 +0300

    исправлено

--- js/script.js
+++ js/script.js
@@ -29,12 +29,23 @@ var link = document.querySelector(".btn-write-us");
 var popup = document.querySelector(".modal-write-us");
 var close = popup.querySelector(".modal-write-us-close");
 
+var form = popup.querySelector("form");
+var writename = popup.querySelector("[name=writename]");
+
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("modal-write-us-show");
+  writename.focus();
 });
 
 close.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("modal-write-us-show");
 });
+window.addEventListener("keydown", function(event) {
+      if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-write-us-show")) {
+          popup.classList.remove("modal-write-us-show");
+        }
+      } 
+    });
